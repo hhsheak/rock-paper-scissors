0d0e463124f26fd09ecc1b81fbf880c027f993b4
hhsheak <239793637+hhsheak@users.noreply.github.com>
2025-11-28 13:46:13 +0800

    finished

--- javascript.js
+++ javascript.js
@@ -1,10 +1,12 @@
+let count = 0
+
 let humanScore = 0
 let computerScore = 0
 
 let computerChoice
-let generated = Math.floor(Math.random()*3)
 
 function getComputerChoice() {
+    let generated = Math.floor(Math.random()*3)
     if (generated === 0) {
         return computerChoice = "ROCK"
     } else if (generated === 1) {
@@ -19,28 +21,45 @@ function getHumanChoice(choice) {
     return humanChoice = choice.toUpperCase()
 }
 
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-        return "Tie!"
-    } else if (humanChoice === "ROCK" && computerChoice === "PAPER") {
-        return "You lose!"
-    } else if (humanChoice === "ROCK" && computerChoice === "SCISSORS") {
-        return "You win!"
-    } else if (humanChoice === "PAPER" && computerChoice === "SCISSORS") {
-        return "You lose!"
-    } else if (humanChoice === "PAPER" && computerChoice === "ROCK") {
-        return "You win!"
-    } else if (humanChoice === "SCISSORS" && computerChoice === "ROCK") {
-        return "You lose!"
-    } else if (humanChoice === "SCISSORS" && computerChoice === "PAPER") {
-        return "You win!"
+while (count < 5) {
+    getComputerChoice()
+    // console.log(computerChoice)
+    
+    getHumanChoice(prompt("Rock! Paper ! Scissors!"))
+    console.log("Your choice: " + humanChoice + ", Computer's choice: " + computerChoice)
+    console.log(playRound(humanChoice, computerChoice))
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === computerChoice) {
+            return "Tie!"
+        } else if (humanChoice === "ROCK" && computerChoice === "PAPER") {
+            computerScore ++
+            return "You lose!"
+        } else if (humanChoice === "ROCK" && computerChoice === "SCISSORS") {
+            humanScore ++
+            return "You win!"
+        } else if (humanChoice === "PAPER" && computerChoice === "SCISSORS") {
+            computerScore ++
+            return "You lose!"
+        } else if (humanChoice === "PAPER" && computerChoice === "ROCK") {
+            humanScore ++
+            return "You win!"
+        } else if (humanChoice === "SCISSORS" && computerChoice === "ROCK") {
+            computerScore ++
+            return "You lose!"
+        } else if (humanChoice === "SCISSORS" && computerChoice === "PAPER") {
+            humanScore ++
+            return "You win!"
+        }
     }
+    console.log("Your score: " + humanScore + ", Computer's score: " + computerScore)
+    console.log(" ")
+    count ++    
 }
 
-getComputerChoice()
-console.log(computerChoice)
-
-getHumanChoice(prompt("Rock! Paper ! Scissors!"))
-console.log(humanChoice)
-
-console.log(playRound(humanChoice,computerChoice))
+if (humanScore > computerScore) {
+    console.log("You win!")
+} else if (humanScore < computerScore) {
+    console.log("You lose!")
+} else {
+    console.log("Tie!")
+}
