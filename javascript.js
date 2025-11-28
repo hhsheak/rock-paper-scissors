3426d32a23e25eec2002d272dbedb719f775f3a1
hhsheak <239793637+hhsheak@users.noreply.github.com>
2025-11-28 13:26:37 +0800

    round

--- javascript.js
+++ javascript.js
@@ -1,16 +1,46 @@
-let choice;
+let humanScore = 0
+let computerScore = 0
+
+let computerChoice
 let generated = Math.floor(Math.random()*3)
-console.log(generated)
 
 function getComputerChoice() {
     if (generated === 0) {
-        return choice = "Rock"
+        return computerChoice = "ROCK"
     } else if (generated === 1) {
-        return choice = "Paper"
+        return computerChoice = "PAPER"
     } else {
-        return choice = "Scissors"
+        return computerChoice = "SCISSORS"
+    }
+}
+
+let humanChoice
+function getHumanChoice(choice) {
+    return humanChoice = choice.toUpperCase()
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        return "Tie!"
+    } else if (humanChoice === "ROCK" && computerChoice === "PAPER") {
+        return "You lose!"
+    } else if (humanChoice === "ROCK" && computerChoice === "SCISSORS") {
+        return "You win!"
+    } else if (humanChoice === "PAPER" && computerChoice === "SCISSORS") {
+        return "You lose!"
+    } else if (humanChoice === "PAPER" && computerChoice === "ROCK") {
+        return "You win!"
+    } else if (humanChoice === "SCISSORS" && computerChoice === "ROCK") {
+        return "You lose!"
+    } else if (humanChoice === "SCISSORS" && computerChoice === "PAPER") {
+        return "You win!"
     }
 }
 
 getComputerChoice()
-console.log(choice)
+console.log(computerChoice)
+
+getHumanChoice(prompt("Rock! Paper ! Scissors!"))
+console.log(humanChoice)
+
+console.log(playRound(humanChoice,computerChoice))
